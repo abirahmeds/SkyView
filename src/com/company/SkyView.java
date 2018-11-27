fccd0ded7544397dbd6b6ae3491fb45f7d9100fd
abirahmeds <43213194+abirahmeds@users.noreply.github.com>
2018-11-27 11:07:32 -0500

    Update SkyView.java

--- src/com/company/SkyView.java
+++ src/com/company/SkyView.java
@@ -1,25 +1,48 @@
 package com.company;
 
 public class SkyView {
-         public SkyView (int numRows, int numCols, double[] scanned)
-        {
-            view = new double[numRows][numCols];
-            int k = 0;
-
-            for (int r = 0; r < numRows; r++)
-            {
-                if (r % 2 = 0){
-                    view[r][c] = scanned[k];
-                    k++;
-
+    private double[][] view;
+    public SkyView(int numRows, int numCols, double[] scanned) {
+        view = new double[numRows][numCols];
+        int scannedNum = 0;
+        for (int x = 0; x < numRows; x++) {
+            if (x % 2 == 0) {
+                for (int y = 0; y < numCols; y++) {
+                    view[x][y] = scanned[scannedNum];
+                    scannedNum++;
                 }
-                else {
-                    for (int c = numCols - 1; c >= 0; c--)
-                    {
-                        view[r][c] = scanned[k];
-                        k++;
-                    }
+            } else {
+                int pos = 0;
+                for (int z = scannedNum - 1 + numCols; z >= numCols * x; z--) {
+                    view[x][pos] = scanned[z];
+                    pos++;
+                    scannedNum++;
                 }
             }
         }
+    }
+
+
+    public double getAverage(int startRow, int endRow, int startCol, int endCol) {
+        double sum = 0;
+        int items = 0;
+        for (int x = startRow; x <= endRow; x++) {
+            for (int y = startCol; y <= endCol; y++) {
+                sum += view[x][y];
+                items++;
+            }
+        }
+        return sum / items;
+    }
+
+    public String toString() {
+        String str = "";
+        for (double[] rowArr : view) {
+            for (double d : rowArr) {
+                str += " " + d;
+            }
+            str += "\n";
+        }
+        return str;
+    }
 }
